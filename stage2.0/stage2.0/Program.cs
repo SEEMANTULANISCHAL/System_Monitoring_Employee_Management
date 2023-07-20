f2f1bed48ca33ed211d4b50dcd00119bf35dace5
S-kiran-k <108232312+S-kiran-k@users.noreply.github.com>
2023-07-20 23:28:24 +0530

    second commit

---
+++ stage2.0/stage2.0/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Win32;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Management;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Connect to MongoDB using Compass connection string
+        string connectionString = "mongodb://localhost:27017/log";
+        MongoClient client = new MongoClient(connectionString);
+        IMongoDatabase database = client.GetDatabase("log");
+        IMongoCollection<BsonDocument> collection = database.GetCollection<BsonDocument>("stage2.0");
+
+        ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_BIOS");
+
+        var serialNumber = string.Empty;
+        var biosAttributes = new BsonDocument();
+        foreach (ManagementObject obj in searcher.Get())
+        {
+
+            string biosSerialNumber = obj["SerialNumber"]?.ToString();
+
+
+            Console.WriteLine("BIOS Serial Number: " + biosSerialNumber);
+            Console.WriteLine();
+
+            // Store the BIOS attributes in the biosAttributes document
+
+            biosAttributes.Add("SerialNumber", biosSerialNumber);
+            serialNumber = biosSerialNumber;
+        }
+        // Query the software component information from the Windows Registry
+        RegistryKey softwareKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall");
+        if (softwareKey != null)
+        {
+            BsonDocument softwareComponentsDocument = new BsonDocument();
+            int counter = 1;
+
+            // Add the BIOS serial number as the first element in the softwareComponentsDocument
+            softwareComponentsDocument.Add("BIOS_SerialNumber", serialNumber);
+
+
+            foreach (string subKeyName in softwareKey.GetSubKeyNames())
+            {
+                using (RegistryKey subKey = softwareKey.OpenSubKey(subKeyName))
+                {
+                    string displayName = subKey.GetValue("DisplayName")?.ToString();
+                    string version = subKey.GetValue("DisplayVersion")?.ToString();
+                    string publisher = subKey.GetValue("Publisher")?.ToString();
+
+                    // Check if any of the values are null
+                    if (displayName != null && version != null && publisher != null)
+                    {
+                        string uniqueKey = GetUniqueKey(displayName, counter);
+
+                        // Create a BsonDocument with software component fields
+                        BsonDocument softwareComponent = new BsonDocument
+                        {
+
+                            { "DisplayName", displayName },
+                            { "Version", version },
+                            { "Publisher", publisher }
+                        };
+
+                        // Use the uniqueKey as the key in the softwareComponentsDocument
+                        softwareComponentsDocument.Add(uniqueKey, softwareComponent);
+
+                        counter++;
+                    }
+                }
+            }
+
+            // Insert the software components document into the MongoDB collection
+            collection.InsertOne(softwareComponentsDocument);
+        }
+
+        Console.WriteLine("Software component information has been retrieved and stored in MongoDB as a single document with a single ID using the display name as the key.");
+        Console.ReadLine();
+    }
+
+    static string GetUniqueKey(string displayName, int counter)
+    {
+        return $"{displayName} ({counter})";
+    }
+}
---
+++ stage2.1/stage2.1/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.Win32;
+using System.Timers;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using System.Management;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Connect to MongoDB using Compass connection string
+
+        string connectionString = "mongodb://localhost:27017/log";
+        MongoClient client = new MongoClient(connectionString);
+        IMongoDatabase database = client.GetDatabase("log");
+        IMongoCollection<SoftwareComponent> collection = database.GetCollection<SoftwareComponent>("Stage2.1");
+        ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_BIOS");
+
+
+        var serialNumber = string.Empty;
+        var biosAttributes = new BsonDocument();
+        foreach (ManagementObject obj in searcher.Get())
+        {
+
+            string biosSerialNumber = obj["SerialNumber"]?.ToString();
+
+
+            Console.WriteLine("BIOS Serial Number: " + biosSerialNumber);
+            Console.WriteLine();
+
+            // Store the BIOS attributes in the biosAttributes document
+
+            biosAttributes.Add("SerialNumber", biosSerialNumber);
+            serialNumber = biosSerialNumber;
+        }
+        // Create a timer to poll for registry changes every 1 second
+        Timer timer = new Timer(1000);
+        timer.Elapsed += (sender, e) =>
+        {
+            // Check if the registry key has changed
+            if (RegistryKeyChanged())
+            {
+                // Retrieve the updated software component information from the registry
+                string displayName = GetRegistryValue("DisplayName");
+                string version = GetRegistryValue("DisplayVersion");
+                string publisher = GetRegistryValue("Publisher");
+
+                // Check if any of the values are null or empty
+                if (!string.IsNullOrEmpty(displayName) && !string.IsNullOrEmpty(version) && !string.IsNullOrEmpty(publisher))
+                {
+                    // Create a SoftwareComponent object
+                    SoftwareComponent softwareComponent = new SoftwareComponent
+                    {
+                        DisplayName = displayName,
+                        Version = version,
+                        Publisher = publisher
+                    };
+
+                    // Insert or update the software component in the MongoDB collection
+                    var filter = Builders<SoftwareComponent>.Filter.Eq("DisplayName", displayName);
+                    collection.ReplaceOne(filter, softwareComponent, new ReplaceOptions { IsUpsert = true });
+                }
+            }
+        };
+
+        // Start the timer
+        timer.Start();
+
+        Console.WriteLine("Registry change monitoring has started. Press any key to stop...");
+        Console.ReadLine();
+
+        // Stop the timer
+        timer.Stop();
+
+        Console.WriteLine("Registry change monitoring has stopped.");
+        Console.ReadKey();
+    }
+
+    // Check if the registry key has changed by comparing the last write time
+    static bool RegistryKeyChanged()
+    {
+        RegistryKey registryKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall");
+        DateTime lastWriteTime = GetRegistryKeyLastWriteTime(registryKey);
+        return lastWriteTime != DateTime.MinValue;
+    }
+
+    // Get the last write time of the registry key
+    static DateTime GetRegistryKeyLastWriteTime(RegistryKey registryKey)
+    {
+        object value = registryKey.GetValue("DisplayName");
+        if (value != null)
+        {
+            RegistryValueKind valueKind = registryKey.GetValueKind("DisplayName");
+            if (valueKind == RegistryValueKind.String)
+            {
+                return (DateTime)registryKey.GetValue("LastWriteTime");
+            }
+        }
+        return DateTime.MinValue;
+    }
+
+    // Get the value of a specific registry key value
+    static string GetRegistryValue(string valueName)
+    {
+        RegistryKey registryKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall");
+        return registryKey?.GetValue(valueName)?.ToString();
+    }
+    class SoftwareComponent
+    {
+        public string DisplayName { get; set; }
+        public string Version { get; set; }
+        public string Publisher { get; set; }
+    }
+
+}
